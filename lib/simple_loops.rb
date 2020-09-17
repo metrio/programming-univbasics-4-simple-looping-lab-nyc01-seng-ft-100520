80509ba878dd669563f452456c5071c0d9504648
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-17 13:28:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,10 @@
 # Write your methods here
 
-def loop_message_five_times
+def loop_message_five_times(message)
   count = 0
+  message = "Hello World"
   while count < 5
-  print "Hello World"
+  print message
   count += 1 
   end
 end
